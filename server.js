17006ad2a141b384ce6be7698e937a87f60da7fd
leslie-v1 <bighommie360@gmail.com>
2025-11-20 12:29:03 +0000

    file structure setup

--- server.js
+++ server.js
@@ -16,6 +16,20 @@ app.get('/old-page(.html)?', (req, res) => {
     res.redirect(301, '/new-page.html'); //302 by default
 });
 
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
 // Route handlers
 app.get('/hello(.html)?', (req, res, next) => {
     console.log('attempted to load hello.html');
@@ -25,23 +39,6 @@ app.get('/hello(.html)?', (req, res, next) => {
 });
 
 
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
 
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
